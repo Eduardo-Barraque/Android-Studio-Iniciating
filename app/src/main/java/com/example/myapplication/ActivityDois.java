2028b868542197378f18b778b5ff649655c5cb0f
Eduardo-Barraque <eduabarraque@gmail.com>
2023-09-26 13:59:35 -0300

    Redirect for other pages

--- app/src/main/java/com/example/myapplication/ActivityDois.java
+++ app/src/main/java/com/example/myapplication/ActivityDois.java
@@ -1,17 +1,32 @@
 package com.example.myapplication;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 import androidx.annotation.Nullable;
 
 public class ActivityDois extends Activity {
     @Override
-    protected void onCreate(@Nullable Bundle savedInstanceState){
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.layout_tela2);
         TextView textViewNomeUsuario = findViewById(R.id.telinha2);
         textViewNomeUsuario.setText(getString(R.string.app_name));
+
+    Button button = findViewById(R.id.botao2);
+    button.setOnClickListener(new View.OnClickListener()
+
+    {
+        @Override
+        public void onClick (View view){
+        Intent intent = new Intent(ActivityDois.this, MinhaActivity.class);
+        startActivity(intent);
+    }
+    });
     }
 }
+
